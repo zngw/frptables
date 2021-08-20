8f83118509e77dc9957835ef02f8f2b85fa4780f
guoke <guoke3915@163.com>
2021-08-21 01:47:53 +0800

    实现tail监听日志测试

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/zngw/frptables
+
+go 1.14
+
+require (
+	github.com/hpcloud/tail v1.0.0
+	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+// @Title
+// @Description $
+// @Author  55
+// @Date  2021/8/21
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hpcloud/tail"
+)
+
+func main() {
+	fileName := "./log.txt"
+	config := tail.Config{
+		ReOpen:    true,                                 // 重新打开
+		Follow:    true,                                 // 是否跟随
+		Location:  &tail.SeekInfo{Offset: 0, Whence: 2}, // 从文件的哪个地方开始读
+		MustExist: false,                                // 文件不存在不报错
+		Poll:      true,
+	}
+	tails, err := tail.TailFile(fileName, config)
+	if err != nil {
+		fmt.Println("tail file failed, err:", err)
+		return
+	}
+	var (
+		line *tail.Line
+		ok   bool
+	)
+	for {
+		line, ok = <-tails.Lines
+		if !ok {
+			fmt.Printf("tail file close reopen, filename:%s\n", tails.Filename)
+			time.Sleep(time.Second)
+			continue
+		}
+		fmt.Println("line:", line.Text)
+	}
+}
